9a528cafee3d3ecde0ec884775a80ff8c9573100
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2024-07-29 22:21:01 -0600

    app architecture added

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import { Footer } from "../../components/shared/Footer/Footer";
+import { Header } from "../../components/shared/Header/Header";
+
+export const HomePage: React.FC = () => {
+  return (
+    <>
+      <Header /> <section>home</section> <Footer />
+    </>
+  );
+};
